a3d50a639e133f14269e5887980ebc282fa96b82
Darko Mijatovic <95342963+Darko96@users.noreply.github.com>
2023-03-29 20:26:20 +0200

    Add files via upload

---
+++ README.md
@@ -0,0 +1,75 @@
+# Frontend Mentor - Calculator app solution
+
+This is a solution to the [Calculator app challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/calculator-app-9lteq5N29). Frontend Mentor challenges help you improve your coding skills by building realistic projects.
+
+## Table of contents
+
+- [Overview](#overview)
+  - [The challenge](#the-challenge)
+  - [Screenshot](#screenshot)
+  - [Links](#links)
+- [My process](#my-process)
+  - [Built with](#built-with)
+  - [What I learned](#what-i-learned)
+  - [Continued development](#continued-development)
+  - [Useful resources](#useful-resources)
+- [Author](#author)
+- [Acknowledgments](#acknowledgments)
+
+## Overview
+
+### The challenge
+
+Users should be able to:
+
+- See the size of the elements adjust based on their device's screen size
+- Perform mathmatical operations like addition, subtraction, multiplication, and division
+- Adjust the color theme based on their preference
+- **Bonus**: Have their initial theme preference checked using `prefers-color-scheme` and have any additional changes saved in the browser
+
+### Screenshot
+
+![](src/img/screenshots/screenshot-1.png)
+![](src/img/screenshots/screenshot-2.png)
+
+### Links
+
+- Solution URL: [Add solution URL here]
+- Live Site URL: [Add live site URL here]
+
+## My process
+
+### Built with
+
+- Semantic HTML5 markup
+- CSS custom properties
+- Flexbox
+- Desktop-first workflow
+
+### What I learned
+
+I learned how to make and change themes for my calculator app.
+
+### Continued development
+
+I'll keep practising javascript, so I can make even bigger projects and pripare myself for React.
+
+### Useful resources
+
+- [A Complete Guide to Grid](https://css-tricks.com/snippets/css/complete-guide-grid/) - This helped me a lot ot understand CSS Grid.
+- [A Complete Guide to Flexbox](https://css-tricks.com/snippets/css/a-guide-to-flexbox/) - This helped me a lot ot understand flex-box.
+- [A Complete Guide to CSS Media Queries](https://css-tricks.com/a-complete-guide-to-css-media-queries/) - This helped me a lot to understand media queries.
+- [JavaScript MDN Documentation](https://developer.mozilla.org/en-US/docs/Web/JavaScript) - This helped me a lot to understand javascript and DOM.
+
+## Author
+
+- Portfolio [Darko Mijatovic](https://darko96.github.io/portfolio/)
+- LinkedIn - [Darko Mijatovic](https://www.linkedin.com/in/darko-mijatovic-512384231/)
+- Frontend Mentor - [@Darko96](https://www.frontendmentor.io/profile/Darko96)
+- Instagram - [darkomiijatovic](https://www.instagram.com/darkomiijatovic/)
+
+## Acknowledgments
+
+I want to thank a lot to everyone in Frontend Mentor team. Because they made an awesome website where we can practice what we learn.
+
+Thank you so much for checking my project and Any feedback is highly appreciated.
---
+++ js/script.js
@@ -0,0 +1,128 @@
+const previousOperand = document.querySelector(".previous-operand");
+const currentOperand = document.querySelector(".current-operand");
+const deleteBtn = document.querySelector(".delete-btn");
+const resetBtn = document.querySelector(".reset-btn");
+const outputBtn = document.querySelector(".output-btn");
+const numBtns = document.querySelectorAll(".num-btn");
+const operators = document.querySelectorAll(".operator-btn");
+let prevOperand = previousOperand.innerText;
+let currOperand = currentOperand.innerText;
+let theme1 = document.getElementById("theme1");
+let theme2 = document.getElementById("theme2");
+let theme3 = document.getElementById("theme3");
+let operation;
+
+// Event listeners for chaning themes
+theme1.addEventListener("click", () => {
+  document.documentElement.classList.remove("theme2");
+  document.documentElement.classList.remove("theme3");
+});
+
+theme2.addEventListener("click", () => {
+  document.documentElement.classList.remove("theme1");
+  document.documentElement.classList.remove("theme3");
+  document.documentElement.classList.toggle("theme2");
+});
+
+theme3.addEventListener("click", () => {
+  document.documentElement.classList.remove("theme1");
+  document.documentElement.classList.remove("theme2");
+  document.documentElement.classList.toggle("theme3");
+});
+
+// Reset function
+function reset() {
+  prevOperand = "";
+  currOperand = "";
+  operation = undefined;
+}
+
+// Delete function
+function deleteOperand() {
+  currOperand = currOperand.toString().slice(0, -1);
+}
+
+// add number function
+function addNumber(number) {
+  if (number == "." && prevOperand.includes(".")) return;
+  currOperand = currOperand.toString() + number.toString();
+}
+
+// Selecting operation function
+function operationSelection(operate) {
+  if (operate == "") return;
+  if (operate !== "") {
+    calculatorOperation();
+  }
+  operation = operate;
+  prevOperand = currOperand;
+  currOperand = "";
+}
+
+// Calculate numbers function
+function calculatorOperation() {
+  let result;
+  let prev = parseFloat(prevOperand);
+  let current = parseFloat(currOperand);
+
+  switch (operation) {
+    case "+":
+      result = prev + current;
+      break;
+    case "-":
+      result = prev - current;
+      break;
+    case "x":
+      result = prev * current;
+      break;
+    case "/":
+      result = prev / current;
+      break;
+    default:
+      return;
+  }
+  currOperand = result;
+  operation = undefined;
+  prevOperand = "";
+  previousOperand.innerHTML = "";
+}
+
+// Display numbers function
+function displayNum() {
+  currentOperand.innerHTML = currOperand.toString();
+  if (operation !== undefined) {
+    previousOperand.innerHTML = `${prevOperand} ${operation}`;
+  } else {
+    previousOperand.innerHTML = prevOperand;
+  }
+}
+
+// Event listeners for numbers, operators, output, delete and reset buttons
+numBtns.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    addNumber(btn.innerHTML);
+    displayNum();
+  });
+});
+
+operators.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    operationSelection(btn.innerHTML);
+    displayNum();
+  });
+});
+
+outputBtn.addEventListener("click", () => {
+  calculatorOperation();
+  displayNum();
+});
+
+deleteBtn.addEventListener("click", () => {
+  deleteOperand();
+  displayNum();
+});
+
+resetBtn.addEventListener("click", () => {
+  reset();
+  displayNum();
+});
